cb28e0a9d61c7a1b25080b2f9dc07cc5834ed7bf
Eric Charnesky <eric.charnesky@careevolution.com>
2021-05-13 20:35:17 -0400

    project 0

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "Eric Charnesky";
+echo "</br>";
+echo "He/Him";
+echo "</br>";
+echo "D. Eng";
+echo "</br>";
+echo "I'm the teacher dummy";
+echo "</br>";
+echo "For you fine folks to learn some PHP and JSP, and maybe love web dev work";
+echo "</br>";
+echo "I love cats!";
+echo "</br>";
+
